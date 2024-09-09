825f69a6a7d9d729c8309d0b2432dd5b0c0b2a1b
木辛木杉 <1210718010@qq.com>
2024-09-09 11:25:22 +0800

    Update image-resizer.py

--- image-resizer.py
+++ image-resizer.py
@@ -20,24 +20,22 @@ def resizer(path):
         return send_from_directory(app.static_folder, out_path)
     else:
         image = f'./{path.split("@")[0]}'
-        parameter = path.split('@')[1]
+        parameter = path.split('@')[1].split('.')[0]
+        fileEx = path.split('@')[1].split('.')[1]
         if 'w' in parameter and 'h' in parameter:
             out_w = int(parameter.split('_')[0].replace('w', ''))
             out_h = int(parameter.split('_')[1].split('.')[0].replace('h', ''))
-            fileEx = parameter.split('.')[1]
             img = Image.open(image)
             resized = img.resize((out_w, out_h), Image.BILINEAR)
             resized.save(f'./static/{out_path}')
         elif 'w' in parameter:
             out_w = int(parameter.split('.')[0].replace('w', ''))
-            fileEx = parameter.split('.')[1]
             img = Image.open(image)
             out_h = round(out_w*img.height/img.width)
             resized = img.resize((out_w, out_h), Image.BILINEAR)
             resized.save(f'./static/{out_path}')
         elif 'h' in parameter:
             out_h = int(parameter.split('.')[0].replace('h', ''))
-            fileEx = parameter.split('.')[1]
             img = Image.open(image)
             out_w = round(out_h*img.width/img.height)
             resized = img.resize((out_w, out_h), Image.BILINEAR)
