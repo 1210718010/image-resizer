6ac4579b83a96c3f9ca757800d9f305901f7531a
木辛木杉 <1210718010@qq.com>
2024-09-10 02:24:22 +0800

    Update image-resizer.py

--- image-resizer.py
+++ image-resizer.py
@@ -8,79 +8,64 @@ app = Flask(__name__, static_folder='static')
 
 @app.route('/<path:path>', methods=['GET', 'POST'])
 
-#   格式，参考B站：
-#   http://127.0.0.1:10000/example.png@.avif
-#   http://127.0.0.1:10000/example.jpg@128w.webp
+#   格式参考B站，示例：
+#   http://127.0.0.1:10000/example.ico@.webp
+#   http://127.0.0.1:10000/example.gif@128w.avif
 #   http://127.0.0.1:10000/example.png@256h.jpg
-#   http://127.0.0.1:10000/example.jpg@512w_512h.png
-#   http://127.0.0.1:10000/example.png@300w_400h.gif
-#   http://127.0.0.1:10000/example.jpg@64w_64h.ico
+#   http://127.0.0.1:10000/example.jpg@512w.png
+#   http://127.0.0.1:10000/example.avif@.gif
+#   http://127.0.0.1:10000/example.webp@64w.ico
 
 def resizer(path):
+
     out_path = path.replace('/', '_')
+
+    fileEx = path.split('@')[1].split('.')[1]
+    if fileEx.lower() == 'webp':
+        io_type = 'WebP'
+        io_mimetype = 'image/webp'
+    elif fileEx.lower() == 'avif':
+        io_type = 'AVIF'
+        io_mimetype = 'image/avif'
+    elif fileEx.lower() == 'jpg' or fileEx.lower() == 'jpeg':
+        io_type = 'JPEG'
+        io_mimetype = 'image/jpeg'
+    elif fileEx.lower() == 'png':
+        io_type = 'PNG'
+        io_mimetype = 'image/png'
+    elif fileEx.lower() == 'gif':
+        io_type = 'GIF'
+        io_mimetype = 'image/gif'
+    elif fileEx.lower() == 'ico':
+        io_type = 'ICO'
+        io_mimetype = 'image/x-icon'
+
     if os.path.isfile(f'./static/{out_path}'):
         os.utime(f'./static/{out_path}', None)
-        fileEx = path.split('@')[1].split('.')[1]
-        if fileEx.lower() == 'avif':
-            mimetype = 'image/avif'
-        elif fileEx.lower() == 'webp':
-            mimetype = 'image/webp'
-        elif fileEx.lower() == 'jpg' or fileEx.lower() == 'jpeg':
-            mimetype = 'image/jpeg'
-        elif fileEx.lower() == 'png':
-            mimetype = 'image/png'
-        elif fileEx.lower() == 'gif':
-            mimetype = 'image/gif'
-        elif fileEx.lower() == 'ico':
-            mimetype = 'image/x-icon'
-        return send_from_directory(app.static_folder, out_path, mimetype=mimetype)
+        return send_from_directory(app.static_folder, out_path, mimetype=io_mimetype)
+
     else:
         image = f'./{path.split("@")[0]}'
         parameter = path.split('@')[1].split('.')[0]
         fileEx = path.split('@')[1].split('.')[1]
-        if 'w' in parameter and 'h' in parameter:
-            out_w = int(parameter.split('_')[0].replace('w', ''))
-            out_h = int(parameter.split('_')[1].split('.')[0].replace('h', ''))
-            img = Image.open(image)
-            resized = img.resize((out_w, out_h), Image.BILINEAR)
-            resized.save(f'./static/{out_path}')
-        elif 'w' in parameter:
-            out_w = int(parameter.split('.')[0].replace('w', ''))
-            img = Image.open(image)
-            out_h = round(out_w*img.height/img.width)
-            resized = img.resize((out_w, out_h), Image.BILINEAR)
-            resized.save(f'./static/{out_path}')
+
+        img = Image.open(image)
+        if 'w' in parameter:
+            out_w = int(parameter.replace('w', ''))
+            if out_w < img.width:
+                out_h = round(out_w*img.height/img.width)
+                img = img.resize((out_w, out_h), Image.BILINEAR)
+                img.save(f'./static/{out_path}')
         elif 'h' in parameter:
-            out_h = int(parameter.split('.')[0].replace('h', ''))
-            img = Image.open(image)
-            out_w = round(out_h*img.width/img.height)
-            resized = img.resize((out_w, out_h), Image.BILINEAR)
-            resized.save(f'./static/{out_path}')
-        else:
-            resized = Image.open(image)
-            resized.save(f'./static/{out_path}')
+            out_h = int(parameter.replace('w', ''))
+            if out_h < img.height:
+                out_w = round(out_h*img.width/img.height)
+                img = img.resize((out_w, out_h), Image.BILINEAR)
+                img.save(f'./static/{out_path}')
 
-        if fileEx.lower() == 'avif':
-            io_type = 'AVIF'
-            io_mimetype = 'image/avif'
-        elif fileEx.lower() == 'webp':
-            io_type = 'WebP'
-            io_mimetype = 'image/webp'
-        elif fileEx.lower() == 'jpg' or fileEx.lower() == 'jpeg':
-            io_type = 'JPEG'
-            io_mimetype = 'image/jpeg'
-        elif fileEx.lower() == 'png':
-            io_type = 'PNG'
-            io_mimetype = 'image/png'
-        elif fileEx.lower() == 'gif':
-            io_type = 'GIF'
-            io_mimetype = 'image/gif'
-        elif fileEx.lower() == 'ico':
-            io_type = 'ICO'
-            io_mimetype = 'image/x-icon'
-
+        img.save(f'./static/{out_path}')
         image_io = BytesIO()
-        resized.save(image_io, io_type)
+        img.save(image_io, io_type)
         image_io.seek(0)
         return send_file(image_io, mimetype=io_mimetype)
 
