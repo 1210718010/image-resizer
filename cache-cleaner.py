80e7a86d43528739d6cdd04f6f7a03348d0299e3
木辛木杉 <1210718010@qq.com>
2024-09-11 09:12:04 +0800

    Update cache-cleaner.py

--- cache-cleaner.py
+++ cache-cleaner.py
@@ -2,18 +2,31 @@ import os
 import time
 import datetime
 
+print('cache-cleaner\n')
+
 while True:
-        dirToBeEmptied = './static/'
+
+    dirToBeEmptied = './static/'
  
-        ds = list(os.walk(dirToBeEmptied))
-        delta = datetime.timedelta(days=14)
-        now = datetime.datetime.now()
+    ds = list(os.walk(dirToBeEmptied))
+    delta = datetime.timedelta(days=14)
+    now = datetime.datetime.now()
+
+    n = 0
  
-        for d in ds:
-            os.chdir(d[0])
-            if d[2] != []:
-                for x in d[2]:
-                    ctime = datetime.datetime.fromtimestamp(os.path.getmtime(x))
-                    if ctime < (now-delta):
-                        os.remove(x)
-        time.sleep(3600)
+    for d in ds:
+        os.chdir(d[0])
+        if d[2] != []:
+            for x in d[2]:
+                ctime = datetime.datetime.fromtimestamp(os.path.getmtime(x))
+                if ctime < (now-delta):
+                    os.remove(x)
+                    n += 1
+
+    print(datetime.datetime.now().strftime('%Y.%m.%d %A %H:%M:%S'), end=' - ')
+    if n == 0:
+        print('没有已过期的缓存')
+    else:
+        print(f'共删除了{n}张缓存图片')
+
+    time.sleep(3600)
